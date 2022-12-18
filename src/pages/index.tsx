346e545a564fe83a4764317383a3c29bd6a2ec9d
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2022-12-18 22:54:49 +0700

    Test GitHub Actions workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Baileys Tutorial
           </Link>
         </div>
       </div>
